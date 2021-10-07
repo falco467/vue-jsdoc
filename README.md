c4041c317ced1091eee1ddb16d733f13117a1bc7
Samuel Burkhard <sxburk@gmail.com>
2021-10-07 18:27:18 +0200

    export lib 💃

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # vue-jsdoc
 An example project for using JSDoc with Vue.JS, so developers get full IntelliSense support in VS Code without having to use TypeScript.
 
-## Mono Repo - Setup
+## Setup
 
 js-vue - vue javascript
 ```
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
+// https://vitejs.dev/config/#config-intellisense
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+      lib: {
+        entry: path.resolve(__dirname, 'src/lib.js'),
+        formats: ['es'],
+    },
+  }
 })
