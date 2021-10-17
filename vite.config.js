2cf5b31155a2d904789747d4c62775e9c347adeb
Samuel Burkhard <sxburk@gmail.com>
2021-10-17 17:11:37 +0200

    lint files

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,9 @@ import path from 'path'
 export default defineConfig({
   plugins: [vue()],
   build: {
-      lib: {
-        entry: path.resolve(__dirname, 'src/lib.js'),
-        formats: ['es'],
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib.js'),
+      formats: ['es'],
     },
-  }
+  },
 })
